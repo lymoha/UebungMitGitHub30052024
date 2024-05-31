6c3506dda113a181eea4cbde0f8f5f75b788fdc4
Mohammed Ly <lybhoye@yahoo.com>
2024-06-01 01:12:34 +0200

    Hi Momo

--- ASimpleJavaProject/src/testMe/ASimpleJavaTestClass.java
+++ ASimpleJavaProject/src/testMe/ASimpleJavaTestClass.java
@@ -4,7 +4,8 @@ public class ASimpleJavaTestClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hi There!");
-
+		System.out.println("Hallo Halli!!!");
+System.out.println("Wie geht es Momo");
 	}
 
 }
